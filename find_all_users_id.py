422b73e260c34f9a36b2a7daf962ce32ccd3c4f8
Aktam <94_aktam@mail.ru>
2022-04-21 07:39:33 +0500

    1

--- find_all_users_id.py
+++ find_all_users_id.py
@@ -1,5 +1,6 @@
 from read_data import read_data
-
+from pprint import pprint 
+import json
 def find_all_users_id(data: dict)->list:
     """ 
     This function will find all the users in the json file and return the list of users id
@@ -8,5 +9,18 @@ def find_all_users_id(data: dict)->list:
         data (dict): Dictionary containing the data of the json file
     Returns:
         list: List containing all the users id
+        
     """
-    return
+    foydalanuvchilar_id_raqamlari=[]
+    foydalanuvchilar=data["messages"]
+    for foydalanuvchi in foydalanuvchilar:
+        foydalanuvchi_id_raqami=foydalanuvchi["id"]
+        foydalanuvchilar_id_raqamlari.append(foydalanuvchi_id_raqami)
+    
+    
+    return foydalanuvchilar_id_raqamlari
+
+data=open("data\result.json")
+data.read()
+data=json.loads(data)
+pprint(find_all_users_id(data))
