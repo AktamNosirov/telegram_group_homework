4828267af616ce02c222948cbcbbbc6c5bd5b866
Aktam <94_aktam@mail.ru>
2022-04-22 18:20:50 +0500

    2

--- find_all_users_name.py
+++ find_all_users_name.py
@@ -8,19 +8,19 @@ def find_all_users_name(data: dict)->list:
         data (dict): Dictionary containing the data of the json file.
     Returns:
         list: List containing all the users name.
-    """
+    """     
     all_users_name=[]
     items=data["messages"]
     for item in items :
         if item.get("actor") :
             user=item.get("actor")
             all_users_name.append(user)
-        if item.get("from") :
-            user=item.get("from")
-            all_users_name.append(user)
+        # if item.get("from") :
+        #     user=item.get("from")
+        #     all_users_name.append(user)
         
     return list(set(all_users_name))
-   
+    
 
 data=open("data/result.json",'r', encoding="UTF8").read()
 data=json.loads(data) 
