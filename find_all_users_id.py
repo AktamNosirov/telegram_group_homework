c221b1bc02fe03ae989bbad7fce54bfcad60c72d
Aktam <94_aktam@mail.ru>
2022-04-22 16:08:14 +0500

    1

--- find_all_users_id.py
+++ find_all_users_id.py
@@ -9,16 +9,27 @@ def find_all_users_id(data: dict)->list:
         list: List containing all the users id
         
     """
-    foydalanuvchilar_id_raqamlari=[]
-    foydalanuvchilar=data["messages"]
+    all_users_id=[]
+    items=data["messages"]
+    for item in items :
+        if item.get("actor_id") :
+            user=item.get("actor_id")
+            all_users_id.append(user)
+        if item.get("from_id") :
+            user=item.get("from_id")
+            all_users_id.append(user)
+            
+    return len(list(set(all_users_id)))
+    # foydalanuvchilar_id_raqamlari=[]
+    # foydalanuvchilar=data["messages"]
     
-    for foydalanuvchi in foydalanuvchilar:
-        if foydalanuvchi.get("from_id") and foydalanuvchi["from_id"] not in foydalanuvchilar_id_raqamlari: 
-            foydalanuvchi_id_raqami=foydalanuvchi["from_id"]
-            foydalanuvchilar_id_raqamlari.append(foydalanuvchi_id_raqami)
-        # if foydalanuvchi.get("actor_id") and foydalanuvchi["actor_id"] not in foydalanuvchilar_id_raqamlari:   
-        #     foydalanuvchi_id_raqami=foydalanuvchi["actor_id"]
-        #     foydalanuvchilar_id_raqamlari.append(foydalanuvchi_id_raqami)
+    # for foydalanuvchi in foydalanuvchilar:
+    #     if foydalanuvchi.get("from_id") and foydalanuvchi["from_id"] not in foydalanuvchilar_id_raqamlari: 
+    #         foydalanuvchi_id_raqami=foydalanuvchi["from_id"]
+    #         foydalanuvchilar_id_raqamlari.append(foydalanuvchi_id_raqami)
+    #     # if foydalanuvchi.get("actor_id") and foydalanuvchi["actor_id"] not in foydalanuvchilar_id_raqamlari:   
+    #     #     foydalanuvchi_id_raqami=foydalanuvchi["actor_id"]
+    #     #     foydalanuvchilar_id_raqamlari.append(foydalanuvchi_id_raqami)
     
     
     return foydalanuvchilar_id_raqamlari
