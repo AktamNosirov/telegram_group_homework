06edcc0c9c872a2dc1b249d516e2e8638aca86d5
Aktam <94_aktam@mail.ru>
2022-04-22 15:52:11 +0500

    1

--- find_all_users_id.py
+++ find_all_users_id.py
@@ -11,16 +11,17 @@ def find_all_users_id(data: dict)->list:
     """
     foydalanuvchilar_id_raqamlari=[]
     foydalanuvchilar=data["messages"]
+    
     for foydalanuvchi in foydalanuvchilar:
-        if foydalanuvchi.get("from_id", False):
+        if foydalanuvchi.get("from_id") and foydalanuvchi["from_id"] not in foydalanuvchilar_id_raqamlari: 
             foydalanuvchi_id_raqami=foydalanuvchi["from_id"]
             foydalanuvchilar_id_raqamlari.append(foydalanuvchi_id_raqami)
-        # else:
+        # if foydalanuvchi.get("actor_id") and foydalanuvchi["actor_id"] not in foydalanuvchilar_id_raqamlari:   
         #     foydalanuvchi_id_raqami=foydalanuvchi["actor_id"]
         #     foydalanuvchilar_id_raqamlari.append(foydalanuvchi_id_raqami)
     
     
-    return list(set(foydalanuvchilar_id_raqamlari))
+    return foydalanuvchilar_id_raqamlari
    
 data=open("data/result.json",'r', encoding="UTF8").read()
 data=json.loads(data) 
